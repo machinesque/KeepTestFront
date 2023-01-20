d6bb8c8de1d17636cf49b3203ddff4c4b7c3a4d5
Luiz Antonio Machinesque <machinesque@gmail.com>
2023-01-20 17:13:24 -0300

    FrontEnd Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'keepTestFront';
+  
+  constructor(private http: HttpClient ) {
+
+  }
+
+  name: string = "";
+  arquivo: any;
+
+  getName(name: string) {
+    this.name = name;
+  }
+
+  inputFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.arquivo = event.target.files[0];
+
+      console.log('file', this.arquivo);
+    }
+  }
+
+  submitData() {
+
+    let formData = new FormData();
+    formData.set("name", this.name);
+    formData.set("file", this.arquivo);
+
+    this.http.post("http://localhost:8080/agentes/arquivo", formData)
+        .subscribe((response) => {});
+
+  }
+
 }
